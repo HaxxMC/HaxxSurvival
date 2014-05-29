02b0bec863223d9a54113dc087f0357e43acf365
Chris <lavacraft219@gmail.com>
2014-05-28 22:00:40 -0400

    Blocked /bukkit: plugin commands. Allowed OPs to use commands.

--- HaxxServer/src/main/java/me/Haxx/HaxxServer/Chat.java
+++ HaxxServer/src/main/java/me/Haxx/HaxxServer/Chat.java
@@ -2,13 +2,13 @@ package me.Haxx.HaxxServer;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class Chat implements Listener {
 
 	@EventHandler
-	public void onChat(AsyncPlayerChatEvent e){
-		if (e.getMessage().startsWith("/bukkit:")){
+	public void onChat(PlayerCommandPreprocessEvent e){
+		if (e.getMessage().startsWith("/bukkit:") && !e.getPlayer().isOp()){
 			e.setCancelled(true);
 			e.getPlayer().sendMessage("§b[§eHaxx§b] §7Most of our plugins are custom made or modified. If you have a question about a plugin, ask a staff member!");
 		}
